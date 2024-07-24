0de3bdd8050e281dbca805d18cf2f1de4a102cb6
Marko Kovac <markokovac153@gmail.com>
2024-07-24 15:07:56 +0100

    updated syntax for controller

--- src/main/java/com/example/API/controller/HotelController.java
+++ src/main/java/com/example/API/controller/HotelController.java
@@ -2,8 +2,8 @@ package com.example.API.controller;
 
 import com.example.API.model.entities.Hotel;
 import com.example.API.service.HotelService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,28 +15,32 @@ public class HotelController {
     HotelService hotelService;
 
     @GetMapping("/hotels")
+    @Operation(summary = "View a list of available hotels")
     public List<Hotel> getAllHotel(){
         return hotelService.getAllHotels();
     }
 
     @GetMapping("/hotel/{id}")
+    @Operation(summary = "Get a hotel by Id")
     public Hotel getHotelById(@PathVariable long id) {
         return hotelService.getHotelById(id);
     }
 
     @PostMapping("/hotel/")
+    @Operation(summary = "Add a hotel")
     public Hotel createHotel(@RequestBody Hotel hotel){
         return hotelService.updateHotel(hotel);
     }
 
     @PutMapping("/hotel/{id}")
+    @Operation(summary = "Update a hotel")
     public Hotel updateHotel(@RequestBody Hotel hotel, @PathVariable long id){
         return hotelService.updateHotel(hotelService.getHotelById(id));
     }
 
     @DeleteMapping("/hotel/{id}")
+    @Operation(summary = "Delete a hotel")
     public void deleteHotel(@PathVariable long id){
         hotelService.deleteHotel(id);
     }
-
 }
--- src/main/java/com/example/API/controller/LandmarkController.java
+++ src/main/java/com/example/API/controller/LandmarkController.java
@@ -2,9 +2,9 @@ package com.example.API.controller;
 
 import com.example.API.model.entities.Landmark;
 import com.example.API.service.LandmarkService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,27 +16,32 @@ public class LandmarkController {
     LandmarkService landmarkService;
 
     @GetMapping("/landmarks")
+    @Operation(summary = "View a list of available landmarks")
     public List<Landmark> getAllLandmarks(){
         return landmarkService.getAllLandmarks();
     }
 
     @GetMapping("/landmark/{id}")
+    @Operation(summary = "Get a landmark by Id")
     public Landmark getLandmarkById(@PathVariable long id) {
         return landmarkService.getLandmarkById(id);
     }
 
     @PostMapping("/landmark/")
+    @Operation(summary = "Add a landmark")
     public Landmark createLandmark(@RequestBody Landmark landmark){
         return landmarkService.updateLandmark(landmark);
     }
 
     @PutMapping("/landmark/{id}")
+    @Operation(summary = "Update a landmark")
     public Landmark updateLandmark(@Parameter(description = "Update landmark", required = true) @RequestBody Landmark landmark, @PathVariable long id){
         landmark.setId(id);
         return landmarkService.updateLandmark(landmark);
     }
 
     @DeleteMapping("/landmark/{id}")
+    @Operation(summary = "Delete a landmark")
     public void deleteLandmark(@PathVariable long id){
         landmarkService.deleteLandmark(id);
     }
